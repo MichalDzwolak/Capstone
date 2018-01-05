4dd274419e7249636d6b023d02a847da1fe8a001
MichalDzwolak <michal.dzwolak@gmail.com>
2018-01-05 07:51:32 +0100

    Update Capstone Project.R

--- Capstone Project.R
+++ Capstone Project.R
@@ -35,12 +35,69 @@ library(tm)
 
 #sampling
 set.seed(1234)
-lineblogss = sample(lineblogs, length(lineblogs)*0.05)
-linenewss = sample(linenews, length(linenews)*0.05)
-linetwitts = sample(linetwitt, length(linetwitt)*0.05)
+lineblogss = sample(lineblogs, length(lineblogs)*0.01)
+linenewss = sample(linenews, length(linenews)*0.01)
+linetwitts = sample(linetwitt, length(linetwitt)*0.01)
 
 data = c(lineblogss, linenewss, linetwitts)
 
-#creating a copus
-datacorpus = VCorpus(VectorSource(data))
-datacorpus
+library(textreg)
+library(tidytext)
+library(dplyr)
+
+corpus <- VCorpus(VectorSource(data))
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+corpus <- tm_map(corpus, toSpace, "(f|ht)tp(s?)://(.*)[.][a-z]+")
+corpus <- tm_map(corpus, toSpace, "@[^\\s]+")
+corpus <- tm_map(corpus, tolower)
+#corpus <- tm_map(corpus, removeWords, stopwords("english"))
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, PlainTextDocument)
+
+#conversion in to character vector
+mytext = convert.tm.to.character(corpus)
+
+unigram = data_frame(text = mytext) %>% 
+  unnest_tokens(word, text) %>% 
+  anti_join(stop_words) %>%
+  group_by(word) %>% 
+  count(word, sort = TRUE) %>%
+  mutate(badword = ifelse(grepl("fuck", word)==TRUE | grepl("œ", word)==TRUE | grepl("â", word)==TRUE,1,0)) %>% filter(badword==0) %>%
+  select(word, n)
+
+#most frequent unigrams graph
+library(ggplot2)
+library(tidyr)
+
+plot1 = ggplot(head(unigram,10), aes(x=reorder(word,-n), y=n)) + 
+  geom_col() + 
+  theme_light() +
+  ylab("Count") + 
+  xlab("Word") + 
+  ggtitle("Most frequent unigrams")
+  
+plot1
+
+#most frequent twograms graph
+twogram = data_frame(text = mytext) %>% 
+  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+  group_by(bigram) %>% 
+  count(bigram, sort = TRUE) %>%
+  mutate(badword = ifelse(grepl("fuck", bigram)==TRUE | grepl("œ", bigram)==TRUE | grepl("â", bigram)==TRUE,1,0)) %>% filter(badword==0) %>%
+  select(bigram, n)
+  
+bigrams_separated = twogram %>% separate(bigram, c("word1", "word2"), sep = " ")
+bigrams_filtered <- bigrams_separated %>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word)
+
+#count new twogram
+bigram_counts <- bigrams_filtered %>% 
+  count(word1, word2, sort = TRUE)
+
+bigrams_united <- bigrams_filtered %>%
+  unite(bigram, word1, word2, sep = " ")
+
+#most frequent twograms graph
