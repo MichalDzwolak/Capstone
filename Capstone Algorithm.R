95eca2240e48667197bd9b6ad2b85229a9e9b506
MichalDzwolak <michal.dzwolak@gmail.com>
2018-01-11 16:04:17 +0100

    Add files via upload

--- Capstone Algorithm.R
+++ Capstone Algorithm.R
@@ -1,26 +1,28 @@
-#Algorithm
-
 library(stringi)
 library(tidyr)
+library(dplyr)
+library(hunspell)
+
 
 bigramtable = read.csv("C:/Users/m.dzwolak/Documents/bigram.csv")
 trigramtable = read.csv("C:/Users/m.dzwolak/Documents/trigram.csv")
-fourgramtable = read.csv("C:/Users/m.dzwolak/Documents/fourgram.csv")
 
 bigramtable$bigram = as.character(bigramtable$bigram)
 trigramtable$trigram = as.character(trigramtable$trigram)
-fourgramtable$fourgram = as.character(fourgramtable$fourgram)
 
+
+#separate
 bigramtable = bigramtable %>% separate(bigram, c("word1", "word2"), sep = " ")
 
 trigramtable = trigramtable %>% separate(trigram, c("word1", "word2", "word3"), sep = " ")
 trigramtable = trigramtable %>% unite(duo, word1, word2, sep = " ")
 
-# Tylko sprawdzam
-# data.frame(File = "Bigram", Numbers = stri_stats_general(bigrams))
-# data.frame(File = "Trigram", Numbers = stri_stats_general(trigram))
 
-input = "welcome to the"
+#Algorithm
+input = "welcme you must"
+split = strsplit(input, " ")
+correct <- hunspell_check(split)
+suggest <- hunspell_suggest(input[!correct])
 
 engine <- function(input){
   
@@ -28,22 +30,43 @@ engine <- function(input){
   
   if(numberofspaces==0){
     bestmatch <- bigramtable %>% filter(bigramtable$word1==input)
-    return(head(bestmatch))
+    output <- head(bestmatch)
+    if(nrow(output)!=0){
+      return(output)
+    }else{
+      print("There is no output!!!!")
+      #tu jest autokorekta
+    }
   }
+  
   if(numberofspaces==1){
     bestmatch <- trigramtable %>% filter(trigramtable$duo==input)
-    return(head(bestmatch))
+    output <- head(bestmatch)
+    if(nrow(output)!=0){
+      return(output)
+    }else{
+      print("There is no output!!!!")
+      #tu musi byc backoff an 2 gram drugiego slowa
+    }
   }
+  
   if(numberofspaces>1){
     modifyinput <- as.data.frame(stri_locate_all(pattern = " ", input, fixed = TRUE))
     modifyinput <- substr(input,modifyinput[1,1]+1,nchar(input))
     bestmatch <- trigramtable %>% filter(trigramtable$duo==modifyinput)
-    return(head(bestmatch))
+    output <- head(bestmatch)
+    
+    if(nrow(output)!=0){
+      return(output)
+    }else{
+      print("There is no output!!!!")
+      #tu musi byc backoff an 2 gram trzeciego slowa
+    }
   }
 }
 
 #aaa <- substr(input,1,2)
-#aaa = gregexpr(" ", input)
+#aaa = strsplit(input, " ")
 
 # modifyinput <- as.data.frame(stri_locate_all(pattern = " ", input, fixed = TRUE))
 # modifyinput <- substr(input,modifyinput[1,1],nchar(input))
